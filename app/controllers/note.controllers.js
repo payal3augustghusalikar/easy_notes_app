cd89150f5b645afd8b64eba8ee3aed1b6fb47d20
payalghusalikar <payal.ghusalikar9@gmail.com>
2020-12-27 23:55:44 +0530

    feat : retriving all and single note

--- app/controllers/note.controllers.js
+++ app/controllers/note.controllers.js
@@ -32,11 +32,43 @@ exports.create = (req, res) => {
 
 // Retrieve and return all notes from the database.
 exports.findAll = (req, res) => {
+    // Retrieve and return all notes from the database.
+exports.findAll = (req, res) => {
+    Note.find()
+    .then(notes => {
+        res.send(notes);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while retrieving notes."
+        });
+    });
+};
 
 };
 
 // Find a single note with a noteId
 exports.findOne = (req, res) => {
+    // Find a single note with a noteId
+exports.findOne = (req, res) => {
+    Note.findById(req.params.noteId)
+    .then(note => {
+        if(!note) {
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.noteId
+            });            
+        }
+        res.send(note);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.noteId
+            });                
+        }
+        return res.status(500).send({
+            message: "Error retrieving note with id " + req.params.noteId
+        });
+    });
+};
 
 };
 
